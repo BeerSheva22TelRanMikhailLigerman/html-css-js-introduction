5819bdf4b26aee94bf40c96014b82f545bfa5b75
LigermanM <Ligerman.m@gmail.com>
2023-05-14 22:12:30 +0300

    color-game

--- src/learn-js.js
+++
@@ -1,7 +0,0 @@
-// let arr = [1, 2, 3];
-// arr.forEach(n  => n = n ** 2);
-// console.log(arr);
-
-function getRandomInrNumber(min, max, minInclusive = true, maxIncusive = false) {
-  return Math.floor(min + Math.random * max +1);
-}
