580a0dbdcc983c3f9f30fdbd5555f3bb5e790072
LigermanM <Ligerman.m@gmail.com>
2023-05-15 11:38:35 +0300

    word.length is variable

--- src/game.js
+++ src/game.js
@@ -1,26 +1,33 @@
 //configuration
 const nTries = 3;
-const vocabulary = ["apple", "comma", "table", "happy", "width", "scope", "floor", "sport", "guess"];
+//const vocabulary = ["apple", "comma", "table", "happy", "width", "scope", "floor", "sport", "guess"];
+const vocabulary = ["a", "bb", "abc", "abcabc"];
+
 //elements
+const SQUARE = '<div id="square-id" class="square"></div>';
+const cellsElement = document.getElementById("cells-id");
 const goButtonElement = document.getElementById("go-id");
 const inputElement = document.getElementById('input-id');
 
-//const squareElement = document.getElementById("square-id");
-const squareElements = document.querySelectorAll(".square");
+//const squareElements = document.querySelectorAll(".square");
+ 
 
 const resultMessageElement = document.getElementById("game-result");
 const playAgainButtonElement = document.getElementById("play-again-id");
 //global variables
 let count = 0;
-let word;
+let word = '';
+let squareElements;
 //functions
 function game() {
     const guess = (inputElement.value).toLowerCase();
-    if (guess.length != 5) {
-        alert("must be 5 letters");            // ? break
+   
+
+    if (guess.length != word.length) {
+        alert(`must be ${word.length} letters`);            // ? break
     } else {
         let victory = 1;
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < word.length; i++) {
         squareElements[i].innerHTML = guess[i];
         if (guess[i] == word[i]) {
             squareElements[i].style.backgroundColor = "green";
@@ -54,9 +61,10 @@ function startGame() {
     word = vocabulary[getRandomIntNumber(0, vocabulary.length)];
     goButtonElement.disabled = false;
     inputElement.readOnly = false;
-    squareElements.forEach(item => {
-        item.style.backgroundColor = "white";
-        item.innerHTML = ' '});
+    // squareElements.forEach(item => {
+    //     item.style.backgroundColor = "white";
+    //     item.innerHTML = ' '});
+    getsquareElements();
     resultMessageElement.innerHTML = '';
     playAgainButtonElement.hidden = true;
 }
@@ -70,6 +78,14 @@ function getRandomIntNumber(min, max, minInclusive = true, maxInclusive = false)
     if (!minInclusive) { min++; }
     if (maxInclusive) { max++; }
     return min < max ? Math.trunc(min + Math.random() * (max - min)) : NaN;
+}
+function getsquareElements(){    
+    // let arr = [word.length];
+    // arr = [...arr];
+    //arr.fill('<div id="square-id" class="square"></div>');
+    
+    cellsElement.innerHTML = SQUARE.repeat(word.length);;
+    squareElements = document.querySelectorAll(".square");    
 }
 
 //Actions
