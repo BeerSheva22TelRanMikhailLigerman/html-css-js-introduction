8b32e1eecb4b0b191b33c48534dd15ac8eed5ffc
LigermanM <Ligerman.m@gmail.com>
2023-05-16 01:25:34 +0300

    function displayOccurrences

---
+++ app.js
@@ -0,0 +1,23 @@
+
+displayOccurrences(["lmn", "ab", "lmn", "c", "d", "ab", "a", "a", "lmn"])
+/*lmn -> 3
+ a -> 2
+ ab -> 2
+ c -> 1
+ d -> 1 */
+
+function displayOccurrences(array) {
+  //array of strings
+  //display strings with their coccurency counts in the descending order of the counts
+  //if counts are equaled then in ascending string values order
+  const obj = {};
+  array.forEach(item => { obj[item] === undefined ? obj[item] = 1 : obj[item]++ });
+  let res = Object.entries(obj);
+  res.sort((a, b) => {return b[1] - a[1] == 0 ? a[0].localeCompare(b[0]) : b[1] - a[1]});  
+  console.log(res);
+}
+
+
+
+
+
