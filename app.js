840454c7d26ae724424ea6643179b9c42ca3c9a2
LigermanM <Ligerman.m@gmail.com>
2023-05-16 10:38:59 +0300

    displayOccurrences  console.log as in the task

--- app.js
+++ app.js
@@ -11,10 +11,10 @@ function displayOccurrences(array) {
   //display strings with their coccurency counts in the descending order of the counts
   //if counts are equaled then in ascending string values order
   const obj = {};
-  array.forEach(item => { obj[item] === undefined ? obj[item] = 1 : obj[item]++ });
+  array.forEach(item => obj[item] === undefined ? obj[item] = 1 : obj[item]++);
   let res = Object.entries(obj);
-  res.sort((a, b) => {return b[1] - a[1] == 0 ? a[0].localeCompare(b[0]) : b[1] - a[1]});  
-  console.log(res);
+  res.sort((a, b) => b[1] - a[1] == 0 ? a[0].localeCompare(b[0]) : b[1] - a[1]);
+  res.forEach(item => console.log(`${item[0]} -> ${item[1]}`));
 }
 
 
