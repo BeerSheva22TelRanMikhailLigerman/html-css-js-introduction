b7b7c83fcba89f4d59156234a3b8c46d18f083d8
LigermanM <Ligerman.m@gmail.com>
2023-05-28 15:37:02 +0300

    WeatherForm,  no validation, not connected to main

--- src/main.js
+++ src/main.js
@@ -1,20 +1,31 @@
 import openMeteoConfig from './config/service-config.json' assert {type: 'json'};
 import OpenMeteoService from './service/OpenMeteoService.js';
+import DataGrid from './ui/DataGrid.js';
+import WeatherForm from './ui/WeatherForm.js';
+import {getISODateStr, getEndDate} from './util/date-functions.js'
+//constants definition
+const columns = [
+    { field: 'date', headerName: 'Date' },
+    { field: 'time', headerName: 'Time' },
+    { field: 'temperature', headerName: 'Temperature' },
+    { field: 'apparentTemperature', headerName: 'Fealt Temp' }
+]
 //functions
-function getISODateStr(date) {
-    return date.toISOString().substring(0,10)
-}
-function getEndDate (startDateStr, days) {
-    const date = new Date(startDateStr);
-    const endDate = new Date(date.setDate(date.getDate() + days));
-    return getISODateStr(endDate)
-}
-const fromFormData = {city: 'Rehovot', 
-startDate: getISODateStr(new Date()), 
-days: 5,hourFrom: 12, hourTo: 14};
+
+
+const fromFormData = {
+    city: 'Rehovot',
+    startDate: getISODateStr(new Date()),
+    days: 0, hourFrom: 20, hourTo: 21
+};
 //objects
+const form = new WeatherForm("form-place",
+ Object.keys(openMeteoConfig.cities), openMeteoConfig.maxDays);
 const openMeteoService = new OpenMeteoService(openMeteoConfig.baseUrl);
+const table = new DataGrid("table-place", columns)
 const latLong = openMeteoConfig.cities[fromFormData.city];
-const {lat, long} = latLong; //object destructing (names must to be equal, order - not)
-const {startDate, days, hourFrom, hourTo} = fromFormData;
-openMeteoService.getTemperatures(lat, long, startDate, getEndDate(startDate, days), hourFrom, hourTo).then(data => console.log(data));
+const { lat, long } = latLong;
+
+const { startDate, days, hourFrom, hourTo } = fromFormData
+openMeteoService.getTemperatures(lat, long, startDate, getEndDate(startDate, days),
+    hourFrom, hourTo).then(data => table.fillData(data))
